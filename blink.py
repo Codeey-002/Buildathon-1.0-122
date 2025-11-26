96c6ea4baf5f68c11cfe31ad42c87c65c47fdaef
Kartik Sud <Codeey-002>
2025-11-26 11:40:22 +0530

    added the EAR logic

--- blink.py
+++ blink.py
@@ -1,5 +1,19 @@
 import cv2
 import mediapipe as mp
+import math
+
+L_EYE = [33,160,158,133,153,144]
+R_EYE = [362,387,385,263,380,373]
+
+def eye_ar(eye_idx, marks):
+    
+    try:
+        A = math.dist(marks[eye_idx[1]], marks[eye_idx[5]])
+        B = math.dist(marks[eye_idx[2]], marks[eye_idx[4]])
+        C = math.dist(marks[eye_idx[0]], marks[eye_idx[3]])
+        return (A + B) / (2 * C)
+    except:
+        return 0
 
 mp_face = mp.solutions.face_mesh
 face = mp_face.FaceMesh()
