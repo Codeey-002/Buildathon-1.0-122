d4ff4705508e0556a27d08a3c652dfbd97f20257
Kartik Sud <Codeey-002>
2025-11-26 13:22:30 +0530

    reads from cmmd txt

--- testui.py
+++ testui.py
@@ -1,17 +1,21 @@
 import streamlit as st
+import os
 from streamlit_autorefresh import st_autorefresh
 
-st_autorefresh(interval=800, key="auto")
+st_autorefresh(interval=800, key="refresh")
 
-st.title("ECHO UI")
+st.title("ECHO UI Test")
 
+cmd = ""
+if os.path.exists("command.txt"):
+    cmd = open("command.txt").read().strip()
+
+st.write("Command received:", cmd)
 
 cols = st.columns(3)
-buttons = [
-    "YES","NO","HELP",
-    "WATER","FOOD","MEDICINE",
-    "CALL NURSE","PAIN","EMERGENCY"
-]
+options = ["YES","NO","HELP",
+           "WATER","FOOD","MEDICINE",
+           "CALL NURSE","PAIN","EMERGENCY"]
 
-for i, b in enumerate(buttons):
-    cols[i%3].button(b)
+for i, x in enumerate(options):
+    cols[i % 3].button(x)
